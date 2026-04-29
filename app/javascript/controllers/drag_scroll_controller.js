8139649f9f5b7e2ec6c40ce42ad77dd75c247ea3
Samet Yılmaz <thefaddyz@gmail.com>
2026-04-29 23:04:07 +0300

    Refine category drag interactions

--- app/javascript/controllers/drag_scroll_controller.js
+++ app/javascript/controllers/drag_scroll_controller.js
@@ -8,33 +8,34 @@ export default class extends Controller {
   start(event) {
     if (event.button !== undefined && event.button !== 0) return
 
-    this.dragging = true
+    this.tracking = true
     this.dragged = false
     this.pointerId = event.pointerId
     this.startX = event.clientX
     this.startScrollLeft = this.element.scrollLeft
-    this.element.classList.add("is-dragging")
-    this.element.setPointerCapture?.(event.pointerId)
   }
 
   move(event) {
-    if (!this.dragging) return
+    if (!this.tracking) return
 
     const delta = event.clientX - this.startX
-    if (Math.abs(delta) > 4) {
+    if (Math.abs(delta) > 8) {
       this.dragged = true
+      this.element.classList.add("is-dragging")
+      this.element.setPointerCapture?.(event.pointerId)
       event.preventDefault()
+      this.element.scrollLeft = this.startScrollLeft - delta
     }
-
-    this.element.scrollLeft = this.startScrollLeft - delta
   }
 
   stop(event) {
-    if (!this.dragging) return
+    if (!this.tracking) return
 
-    this.dragging = false
+    this.tracking = false
     this.element.classList.remove("is-dragging")
-    this.element.releasePointerCapture?.(this.pointerId || event.pointerId)
+    if (this.dragged) {
+      this.element.releasePointerCapture?.(this.pointerId || event.pointerId)
+    }
 
     if (this.dragged) {
       this.element.addEventListener("click", this.cancelClick, { capture: true, once: true })
