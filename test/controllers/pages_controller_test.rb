c921a300c42f06beb13b0d899370d423a0c7ac28
Samet Yılmaz <thefaddyz@gmail.com>
2026-04-29 23:51:41 +0300

    Redesign account settings experience

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -5,6 +5,23 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
     @user = users(:one)
   end
 
+  test "signed in user can view account settings" do
+    sign_in @user
+
+    get account_profile_path
+
+    assert_response :success
+    assert_select "h1", "Profil ve güvenlik"
+    assert_select "#profile-settings"
+    assert_select "#security-settings"
+  end
+
+  test "guest is redirected from account settings" do
+    get account_profile_path
+
+    assert_redirected_to new_user_session_path
+  end
+
   test "signed in user can update profile from account area" do
     sign_in @user
 
@@ -26,5 +43,37 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
     }
 
     assert_redirected_to account_profile_path
+  end
+
+  test "invalid profile update renders account settings with errors" do
+    sign_in @user
+
+    patch account_profile_path, params: {
+      user: {
+        name: "Updated User",
+        email: "updated@example.com",
+        current_password: ""
+      }
+    }
+
+    assert_response :unprocessable_entity
+    assert_select ".account-form-alert", text: /Profil bilgilerini kontrol et/
+    assert_select "#profile-current-password-error"
+  end
+
+  test "invalid password update renders account settings with errors" do
+    sign_in @user
+
+    patch account_password_path, params: {
+      user: {
+        current_password: "wrong-password",
+        password: "short",
+        password_confirmation: "different"
+      }
+    }
+
+    assert_response :unprocessable_entity
+    assert_select ".account-form-alert", text: /Şifre bilgilerini kontrol et/
+    assert_select "#security-current-password-error"
   end
 end
